21ffbeb4762ae84985affb5356aa7e2f2cf9bfe2
LTIUser <LTIUser@INFEDL31192.lntinfotech.com>
2021-09-27 13:59:50 -0500

    Added polish to program

--- Console/Program.cs
+++ Console/Program.cs
@@ -9,27 +9,52 @@ namespace MainConsole
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to the Health Tracker! What would you like to do?");
+            Console.WriteLine("(Type \"help\" to see command options. Type \"quit\" to close the program.)");
+
             while (true)
             {
-                Console.WriteLine("Welcome to the Health Tracker! What would you like to do?\nInput Example - mm/dd/yyy weight bmi calories");
+                Console.WriteLine("Please enter a command.");
+                Console.Write("> ");
                 var input = Console.ReadLine();
                 var splitInput = input.Split(" ");
 
-                if (splitInput[0] == "add")
+                if (splitInput.Count() > 0)
                 {
-                    Sql.Insert(splitInput[1], Int32.Parse(splitInput[2]), Convert.ToDouble(splitInput[3]), Int32.Parse(splitInput[4]));
-                }
-                else if (splitInput[0] == "update")
-                {
-                    Sql.Update(Int32.Parse(splitInput[1]), splitInput[2], Int32.Parse(splitInput[3]),Convert.ToDouble(splitInput[4]), Int32.Parse(splitInput[5]));
-                }
-                else if (splitInput[0] == "delete")
-                {
-                    Sql.Delete(Int32.Parse(splitInput[1]));
-                }
-                else if (splitInput[0] == "query")
-                {
-                    Sql.Query();
+                    if (splitInput[0] == "add" && splitInput.Count() == 5)
+                    {
+                        Sql.Insert(splitInput[1], Int32.Parse(splitInput[2]), Convert.ToDouble(splitInput[3]), Int32.Parse(splitInput[4]));
+                    }
+                    else if (splitInput[0] == "update" && splitInput.Count() == 6)
+                    {
+                        Sql.Update(Int32.Parse(splitInput[1]), splitInput[2], Int32.Parse(splitInput[3]), Convert.ToDouble(splitInput[4]), Int32.Parse(splitInput[5]));
+                    }
+                    else if (splitInput[0] == "delete" && splitInput.Count() == 2)
+                    {
+                        Sql.Delete(Int32.Parse(splitInput[1]));
+                    }
+                    else if (splitInput[0] == "query" && splitInput.Count() == 1)
+                    {
+                        Sql.Query();
+                    }
+                    else if (splitInput[0] == "help" && splitInput.Count() == 1)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("[EXAMPLE INPUTS]");
+                        Console.WriteLine("Query data: query");
+                        Console.WriteLine("Add item: add mm/dd/yyyy weight bmi calories");
+                        Console.WriteLine("Update item: update id mm/dd/yyyy weight bmi calories");
+                        Console.WriteLine("Delete item: delete id");
+                        Console.WriteLine();
+                    }
+                    else if (splitInput[0] == "quit" && splitInput.Count() == 1)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input.");
+                    }
                 }
             }
         }
